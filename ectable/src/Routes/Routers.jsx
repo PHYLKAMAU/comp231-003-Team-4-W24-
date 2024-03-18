f636b9050535f0ea224c575ae0b17b1fd81f1663
phylkamau <phyliskamau3902@gmail.com>
2024-03-18 14:06:15 +0300

    restaurant-owner

---
+++ ectable/src/Routes/Routers.jsx
@@ -0,0 +1,36 @@
+import Home from "../pages/Home";
+import Services from "../pages/Services";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import Contact from "../pages/Contact";
+import Admin from "../pages/Admin/Admin";
+import RestManagement from "../pages/Admin/RestManagement";
+import RestaurantSignup from "../pages/Admin/RestaurantSignup";
+import RestaurantList from "../pages/Restaurants/RestaurantList";
+import ProtectedRoute from "../components/ProtectedRoutes";
+import BookingPage from "../pages/BookingPage";
+
+import { Routes, Route } from "react-router-dom";
+const Routers = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Signup" element={<Signup />} />
+      <Route path="/Login" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/Services" element={<Services />} />
+      <Route path="/Contact" element={<Contact />} />
+      <Route path="/RestaurantList" element={<RestaurantList />} />
+      <Route path="/BookingPage" element={<BookingPage />} />
+
+      <Route element={<ProtectedRoute />}>
+        <Route path="/Admin" element={<Admin />} />
+        <Route path="/RestManagement" element={<RestManagement />} />
+        <Route path="/RestaurantSignup" element={<RestaurantSignup />} />
+        <Route path="/BookingPage/:restaurantId" element={<BookingPage />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default Routers;
---
+++ ectable/src/components/Header/Header.jsx
@@ -0,0 +1,95 @@
+// import { useEffect, useRef } from "react";
+import logo from "../../assets/images/logo.png";
+import { NavLink, Link } from "react-router-dom";
+import { BiMenu } from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
+import { authLogout } from "../Auth/auth";
+const NavLinks = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "/RestaurantList",
+    display: "Restaurants",
+  },
+  {
+    path: "/Signup",
+    display: "Signup",
+  },
+];
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  const logout = () => {
+    authLogout(navigate);
+    console.log("Loggout!");
+  };
+
+  return (
+    <header className="header flex items-center">
+      <div className="container flex items-center justify-between w-full">
+        {/*====logo *=====*/}
+        <div>
+          <img src={logo} alt="logo" />
+        </div>
+
+        {/**========Menu */}
+        <div className="navigation">
+          <ul className="menu flex items-center gap-[2.7rem]">
+            {NavLinks.map((item, index) => (
+              <li key={index}>
+                <NavLink
+                  to={item.path}
+                  className={(navClass) =>
+                    navClass.isActive
+                      ? "text-primary-Color text-[16px] leading-7 font-[600]"
+                      : "text-text-Color text-[16px] leading-7 font-[500] hover:text-primary-Color"
+                  }
+                  activeClassName="active"
+                >
+                  {item.display}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {/*==== Nav Right ====*/}
+        <div className="flex items-center gap-4">
+          <div className="hidden">
+            <Link to="/">
+              <figure className="w-[35px] h-[35px] rounded-full cursor-pointer">
+                <img
+                  src="{userImg}"
+                  alt="user"
+                  className="w-full h-full rounded-full"
+                />
+              </figure>
+            </Link>
+          </div>
+
+          <Link to="/login">
+            <button className="bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center-justify-center rounded-[50px]">
+              Login
+            </button>
+          </Link>
+
+          <button
+            onClick={logout}
+            className="bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center-justify-center rounded-[50px]"
+          >
+            Logout
+          </button>
+
+          <span className="md:hidden">
+            <BiMenu className="w-6 h-6 cursor-pointer" />
+          </span>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
